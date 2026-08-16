063786f190a42d61d5816e4a2b64a7cf4ee3d34c
kenielf <chrystian.keni.github@gmail.com>
2026-08-16 03:28:19 -0300

    refac(lint): formatting; isort now follows gitignore

--- lint.sh
+++ lint.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-black --target-version=py314 cvbuilder && isort --profile=black cvbuilder
+black --target-version=py314 cvbuilder && \
+    isort --profile=black --gitignore cvbuilder
